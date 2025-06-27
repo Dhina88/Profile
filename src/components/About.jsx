58c14e7b722f003229c27bcae680ff70e0a3330c
Dhina <dugxon@gmail.com>
2025-06-27 23:04:56 +0800

    c31

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,13 @@ const About = () => (
         <h2 className={styles.heading}>About Me</h2>
         <p className={styles.text}>
           I'm Dhina, A Tech Enthusiast, Innovator and Engineer who loves building impactful digital experiences. I enjoy solving problems, learning new technologies, and collaborating with creative people.
-          I'm also the founder of Mindflux, a startup I started to explore how AI can be integrated into the real world; not just as a tool, but as a driver of impact. At Mindflux, we're building practical, intelligent systems that solve actual problems and making sure innovation stays grounded in purpose. <br></br><br></br>-Progress, innovation, and real-world impact continue to lead the way-
+          I'm also the Director of Mindflux Technology, a startup I started to explore how AI can be integrated into the real world; not just as a tool, but as a driver of impact. At Mindflux, we're building practical, intelligent systems that solve actual problems and making sure innovation stays grounded in purpose. <br></br><br></br>-Progress, innovation, and real-world impact continue to lead the way-
         </p>
       </div>
       <div className={styles.right}>
         <h3 className={styles.subheading}>Tech Specialist Profile</h3>
         <p className={styles.rightDesc}>
-        A forward-thinking professional who is passionate about solving complex problems and driving meaningful progress through technology. Committed to continuous learning, delivering impactful solutions, and contributing to innovative projects that make a difference.
+        A forward thinking professional who is passionate about solving complex problems and driving meaningful progress through technology. Committed to continuous learning, delivering impactful solutions, and contributing to innovative projects that make a difference.
         </p>
         <ul className={styles.skillsList}>
           <li>Web & App Development</li>
