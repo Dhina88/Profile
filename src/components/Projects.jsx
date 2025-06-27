96d8f5695b6e1435f6f85af1d90eae1680b017e4
Dhina <dugxon@gmail.com>
2025-06-28 02:24:28 +0800

    c47

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,32 +3,32 @@ import styles from '../styles/Projects.module.css';
 
 const projects = [
   {
-    title: 'Web & App Development',
+    title: 'Web & App Development 💻',
     description: 'Custom-built websites and mobile-ready apps designed for speed, responsiveness, and a smooth user experience.',
     link: '#',
   },
   {
-    title: 'AI Integration',
+    title: 'AI Integration 🤖',
     description: 'Smart solutions powered by AI; From automation tools to intelligent features that enhance productivity and real-world impact.',
     link: '#',
   },
   {
-    title: 'System Security Integration',
+    title: 'System Security Integration 🛡️',
     description: 'Security-first systems designed to detect, prevent, and respond to threats, combining secure architecture with real-time monitoring.',
     link: '#',
   },
   {
-    title: 'Forensics Interface Framework',
+    title: 'Forensics Interface Framework 🔍 ',
     description: 'A modular interface toolkit tailored for digital forensics apps—timeline viewers, evidence logs, and file traceability tools.',
     link: '#',
   },
   {
-    title: 'UI Component Library',
+    title: 'UI Component Library 🧩',
     description: 'A modular engine to rapidly prototype and deploy user interfaces with consistent behavior and design patterns.',
     link: '#',
   },
   {
-    title: 'Interface Engine',
+    title: 'Interface Engine ⚙️',
     description: 'A reusable set of secure and scalable UI components built to streamline development and maintain consistency across projects.',
     link: '#',
   },
